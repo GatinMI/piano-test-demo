14d0b98d0b36adf3ebdf726b9decc368d5d3b485
Gatin Marat <marat96g@gmail.com>
2019-03-14 06:17:08 +0300

    remove unused import

--- src/main/java/io/piano/service/impl/QuestionServiceImpl.java
+++ src/main/java/io/piano/service/impl/QuestionServiceImpl.java
@@ -8,7 +8,6 @@ import io.piano.service.QuestionService;
 import io.piano.service.result.QuestionServiceResult;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
